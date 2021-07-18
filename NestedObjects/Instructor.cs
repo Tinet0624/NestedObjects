474a4bf4bbc53364cb68731dccf35841756884d4
Alexandra Haley <72041575+Tinet0624@users.noreply.github.com>
2021-07-18 15:43:23 -0700

    Added Instructor class

---
+++ NestedObjects/Instructor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// The name and email of the Instructor for the courses
+    /// </summary>
+    class Instructor
+    {
+        /// <summary>
+        /// Legal Full name first and last
+        /// ex: Jane Doe
+        /// </summary>
+        public string FullName { get; set; }
+        /// <summary>
+        /// Company email address
+        /// ex: jane.doe@cptc.edu
+        /// </summary>
+        public string Email { get; set; }
+    }
+}
